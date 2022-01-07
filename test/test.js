6144550cb849748135f3c62d6d60c606af74a835
goosy <goosy.jo@gmail.com>
2022-01-07 09:51:06 +0800

    add CallExpr, ConditionalExpr and ChainExpr

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import {convert} from "../src/index.js";
+import { convert } from "../src/index.js";
 import assert from "assert";
 describe('convert(tags, template)', () => {
     describe('模板正确解析', () => {
@@ -8,36 +8,36 @@ describe('convert(tags, template)', () => {
                 assert.strictEqual( // 测试 变量 括号中变量 含变量的字符串
                     convert({
                         "name": "赵六"
-                    }, '人物：{{name}} {{(name)}} {{"name"+name}}'),
-                    "人物：赵六 赵六 name赵六"
+                    }, '人物: {{name}} {{(name)}} {{"name"+name}}'),
+                    "人物: 赵六 赵六 name赵六"
                 );
             });
             it('{{ 非变量 }} 输出 undefined', () => {
                 assert.strictEqual( // 测试 不是变量的标识符
                     convert({
                         "name": "赵六"
-                    }, '人物：{{naem}} {{"name"+naem}}'),
-                    "人物：undefined nameundefined"
+                    }, '人物: {{naem}} {{"name"+naem}}'),
+                    "人物: undefined nameundefined"
                 );
             });
             it('{{ 一元二元运算式 }} 输出正确', () => {
                 assert.strictEqual( // 测试数字的二元运算 + - * / %
                     convert(
-                        {"length": 8, 'width': 6},
-                        '面积:{{(length+3)*(width-4)/2}} \n空余：{{length % width}}'
+                        { "length": 8, 'width': 6 },
+                        '面积:{{(length+3)*(width-4)/2}} \n空余: {{length % width}}'
                     ),
-                    "面积:11 \n空余：2"
+                    "面积:11 \n空余: 2"
                 );
                 assert.strictEqual( // 测试布尔的二元运算 && ||
                     convert(
-                        {"go": true, 'car': false, ud: null},
-                        '步行外出:{{go && !car}} \n乘车外出：{{go && car}} \n有外出：{{go || car}}\n{{ud ?? "无效值"}}'
+                        { "go": true, 'car': false, ud: null },
+                        '步行外出:{{go && !car}} \n乘车外出: {{go && car}} \n有外出: {{go || car}}\n{{ud ?? "无效值"}}'
                     ),
-                    "步行外出:true \n乘车外出：false \n有外出：true\n无效值"
+                    "步行外出:true \n乘车外出: false \n有外出: true\n无效值"
                 );
                 assert.strictEqual( // 测试其它二元运算 == === != !==
                     convert(
-                        { },
+                        {},
                         '{{null == undefined}}:{{null === undefined}}:{{null != undefined}}:{{null !== undefined}}'
                     ),
                     "true:false:false:true"
@@ -48,35 +48,66 @@ describe('convert(tags, template)', () => {
                         "prop": "ID",
                         "conn": {
                             "ID": 2,
-                            "addr": {"ID": 3, "value": "xian rd. 128"}
+                            "addr": { "ID": 3, "value": "xian rd. 128" }
                         }
                     }, '{{ID}} {{conn.ID}} {{conn["ID"]}} {{conn[prop]}} {{conn.addr.ID}} {{conn.addr["value"]}}'),
                     "1 2 2 2 3 xian rd. 128"
+                );
+                assert.strictEqual( // 测试链运算符 ?.
+                    convert({
+                        "someone": {
+                            "addr": { "ID": 3, "value": "xian rd. 128" }
+                        }
+                    }, '{{someone?.addr?.ID}} {{someone["addr"]?.value}} {{someone["addr"]?.zip}}'),
+                    "3 xian rd. 128 undefined"
+                );
+            });
+            it('{{ 三元运算式 }} 输出正确', () => {
+                assert.strictEqual( // 测试赋值运算 '?:'
+                    convert(
+                        { "n": 1, 'b': false },
+                        '{{n==1?"n=1":""}}\n{{b?"b=true":"b=false"}}'
+                    ),
+                    "n=1\nb=false"
                 );
             });
             it('{{ 赋值运算式 }} 输出正确', () => {
                 assert.strictEqual( // 测试赋值运算 '=', '+=', '-=', '*=', '**=', '/=', '%='
                     convert(
-                        {"n": 1, 'b': false},
+                        { "n": 1, 'b': false },
                         '{{n+=7}}{{n}}\n{{b=b||true}}{{b}}\n{{n%=5}}{{n}}\n{{"n:",m=10,m}}'
                     ),
                     "8\ntrue\n3\nn:10"
                 );
                 assert.strictEqual( // 测试赋值运算 '=', '+=', '-=', '*=', '**=', '/=', '%=', '??='
                     convert(
-                        {"n": 1, 'b': false},
+                        { "n": 1, 'b': false },
                         '{{n+=7}}{{n}}\n{{c=b||"on false"}}{{c}}\n{{c=b??"no false"}}{{c}}\n{{n%=5}}{{n}}\n{{"n:",c=10,c}}'
                     ),
                     "8\non false\nfalse\n3\nn:10"
                 );
             });
-            it('{{ range() }} 输出正确', () => {
-                assert.strictEqual( 
+            it('call 输出正确', () => {
+                assert.strictEqual( // {{ range() }} 输出正确'
                     convert(
                         {},
                         '{{range(10)}}\n{{#for i in range(1,11)}}{{i}} {{#endfor}}\n{{#for i in range(10,-1,-2)}}{{i}} {{#endfor}}'
                     ),
                     "0,1,2,3,4,5,6,7,8,9\n1 2 3 4 5 6 7 8 9 10 \n10 8 6 4 2 0 "
+                );
+                assert.strictEqual( // Object.valueOf Object.entries 输出正确'
+                    convert(
+                        { 'myobj': { name: 'myobj', value: 'v' } },
+                        '{{myobj.valueOf()}} {{Object.entries(myobj)}}'
+                    ),
+                    "[object Object] name,myobj,value,v"
+                );
+                assert.strictEqual( // Array.join 输出正确'
+                    convert(
+                        { 'mylist': ['apple', 'banana', 'orange', 'pear'] },
+                        '{{ mylist.join(" | ") }}'
+                    ),
+                    "apple | banana | orange | pear"
                 );
             });
         });
@@ -84,14 +115,14 @@ describe('convert(tags, template)', () => {
             it('#for 数组', () => {
                 assert.strictEqual( // 测试数组遍历
                     convert(
-                        {peoplelist:["张三", "李四", "王五"]}, 
-                        '以下人员:\n{{#for name in peoplelist}}人物：{{name}}\n{{#endfor}}'
+                        { peoplelist: ["张三", "李四", "王五"] },
+                        '以下人员:\n{{#for name in peoplelist}}人物: {{name}}\n{{#endfor}}'
                     ),
-                    "以下人员:\n人物：张三\n人物：李四\n人物：王五\n"
+                    "以下人员:\n人物: 张三\n人物: 李四\n人物: 王五\n"
                 );
                 assert.strictEqual( // 测试数组带序号遍历
                     convert(
-                        {peoplelist:["张三", "李四", "王五"]}, 
+                        { peoplelist: ["张三", "李四", "王五"] },
                         '{{#for sn, name in peoplelist}}人物{{sn+1}}:{{name}}\n{{#endfor sn, name}}'
                     ),
                     '人物1:张三\n人物2:李四\n人物3:王五\n'
@@ -100,14 +131,14 @@ describe('convert(tags, template)', () => {
             it('#for 对象', () => {
                 assert.strictEqual( // 测试对象遍历
                     convert(
-                        {person:{"name":"张三", "age":18, "gender":"男"}}, 
+                        { person: { "name": "张三", "age": 18, "gender": "男" } },
                         '人员:\n{{#for prop in person}}{{prop}}\n{{#endfor}}'
                     ),
                     "人员:\n张三\n18\n男\n"
                 );
                 assert.strictEqual( // 测试对象带键值遍历
                     convert(
-                        {person:{"name":"张三", "age":18, "gender":"男"}}, 
+                        { person: { "name": "张三", "age": 18, "gender": "男" } },
                         '人员:\n{{#for pname, prop in person}}{{pname}}:{{prop}}\n{{#endfor}}'
                     ),
                     "人员:\nname:张三\nage:18\ngender:男\n"
@@ -123,8 +154,8 @@ describe('convert(tags, template)', () => {
         describe('#if condition', () => {
             it('#if condition', () => {
                 assert.strictEqual(
-                    convert({}, ' {{#if "name".length \n>\n0   }}\n人物：{{"XX"}}{{#endif}}'),
-                    " \n人物：XX"
+                    convert({}, ' {{#if "name".length \n>\n0   }}\n人物: {{"XX"}}{{#endif}}'),
+                    " \n人物: XX"
                 );
                 assert.strictEqual(
                     convert({
@@ -148,6 +179,9 @@ describe('convert(tags, template)', () => {
             assert.throws(() => {
                 convert({}, '{{ a++ }}{{ c <<= d }}');
             }, Error);
+            assert.throws(() => {
+                convert({}, '{{b = a => a+1}}');
+            }, Error);
         });
         it('{{#指令 错误', () => {
             assert.throws(() => {
@@ -165,23 +199,23 @@ describe('convert(tags, template)', () => {
         });
         it('#for #endfor 不匹配', () => {
             assert.throws(() => {
-                convert({}, '{{#for name in ["张三", "李四", "王五"]}}人物：{{name}}');
+                convert({}, '{{#for name in ["张三", "李四", "王五"]}}人物: {{name}}');
             }, Error);
             assert.throws(() => {
-                convert({}, '以下人员:{{#if "赵六" in ["张三", "李四", "王五"]}}\n人物：{{name}}{{#endfor}}');
+                convert({}, '以下人员:{{#if "赵六" in ["张三", "李四", "王五"]}}\n人物: {{name}}{{#endfor}}');
             }, Error);
         });
         it('#if #endif 不匹配', () => {
             assert.throws(() => {
-                convert({}, '以下人员:{{#if "赵六" in ["张三", "李四", "王五"]}}\n人物：{{name}}');
+                convert({}, '以下人员:{{#if "赵六" in ["张三", "李四", "王五"]}}\n人物: {{name}}');
             }, Error);
             assert.throws(() => {
-                convert({}, '{{#for name in ["张三", "李四", "王五"]}}人物：{{name}}{{#endif}}');
+                convert({}, '{{#for name in ["张三", "李四", "王五"]}}人物: {{name}}{{#endif}}');
             }, Error);
         });
         it('{{ 与 }} 配套嵌套正确', () => {
             assert.throws(() => {
-                convert({}, '人物：{{name"赵六"');
+                convert({}, '人物: {{name"赵六"');
             }, Error);
         });
     });
